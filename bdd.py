ca314e390a5a167c5bb75d45665e489bda8187f7
pmirandaarias <paulo@afexav.cl>
2020-06-08 18:07:00 -0400

    Se agrega codigo para bdd

---
+++ bdd.py
@@ -0,0 +1,21 @@
+# pass: f+oY5**v3}Q}
+#!/usr/bin/python
+import MySQLdb
+
+db = MySQLdb.connect(host="academiachicureo.cl",
+                     user="academia_jaime",         # your username
+                     passwd="f+oY5**v3}Q}",  # your password
+                     db="academia_jaime")        # name of the data base
+
+# you must create a Cursor object. It will let
+#  you execute all the queries you need
+cur = db.cursor()
+
+# Use all the SQL you like
+cur.execute("SELECT * FROM usuarios")
+
+# print all the first cell of all the rows
+for row in cur.fetchall():
+    print row
+
+db.close()
